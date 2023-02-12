0811c120fac65173ef71a59ccf85758ef61b624e
Jamal Taylor <jtaylor@jtaylorMBP16.lan>
2023-02-12 15:28:00 -0500

    code check in

---
+++ weekThree/Functions/functions.js
@@ -0,0 +1,17 @@
+// Questions 1
+/*  Let's Shop 'Til We Drop!: Write a function named checkout that accepts 2 parameters named cardBalance and price. If the price is more than the cardBalance, return the message "Sorry, can't afford it."; otherwise, return the message
+*    "Thanks for your purchase! Your new card balance is [new balance]."
+
+*   For example, checkout(100, 50) should return "Thanks for your purchase! Your new card balance is 50."
+*   and checkout(100, 200) should return "Sorry, can't afford it."
+*/
+
+// Question 2
+/**
+ * Let's Count Up!: Write a function named loopUntilX that takes one parameter x.
+ * åWithin the function, write a loop that loops x times, starting at 0.
+ *  Within the loop, print the message "Counting up: [current iteration number]!" to the console. Once the loop is done, return the message "Countdown complete! Blast off!"
+ * For example, loopUntilX(5) should print:
+ */
+
+// Question 3
